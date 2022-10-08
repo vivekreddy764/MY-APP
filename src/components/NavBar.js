87f101959f08357613ff59feba7ff4775d5c5398
Saivivek Reddy <32547177+vivekreddy764@users.noreply.github.com>
2022-10-08 14:22:51 -0500

    made changes to app.js and added css fonts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
@@ -25,7 +31,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" > 
         <span className="navbar-toggler-icon"></span>
@@ -49,9 +55,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
               <div className="social-icon">
-                  <a href="#"><img src={''} alt=""/></a>
-                  <a href="#"><img src={''} alt=""/></a>
-                  <a href="#"><img src={''} alt=""/></a>
+                  <a href="#"><img src={navIcon1} alt=""/></a>
+                  <a href="#"><img src={navIcon2} alt=""/></a>
+                  <a href="#"><img src={navIcon3} alt=""/></a>
               </div>
               <button className="vvd" onClick={() => console.log('connect')}><span>Let's connect</span></button>
           </span>
